991cc6d2c3d4ce3bd4cf76510196c796f7911480
johnsonice <huangchengyu16@gmail.com>
2023-05-05 16:03:19 -0400

    update

--- examples/tweet_classification/inference_simple_translated_data.py
+++ examples/tweet_classification/inference_simple_translated_data.py
@@ -57,7 +57,7 @@ ft = Features({'created_at':Value('string'),
     'text':Value('string')})
 #%%
 if __name__ == "__main__":    
-    args = t_args([])
+    args = t_args()
     ### use lattest roberta based twitter sentiment model 
     MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
     data_folder = '/data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks'
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     # chunk_dataset = load_dataset('csv', 
     #                             data_files=Inference_data_dir,)
     # #                            features=ft)
-    in_df = pd.read_csv(Inference_data_dir)
+    in_df = pd.read_csv(Inference_data_dir,dtype=str)
     chunk_dataset = Dataset.from_pandas(in_df)
     #%%
     print(chunk_dataset[10])
@@ -109,33 +109,3 @@ if __name__ == "__main__":
     #df.to_pickle(os.path.join(data_folder,'climate_awareness_agg_senti.parquet'))
     #%%
     
-    #%%
-    # tokenizer = AutoTokenizer.from_pretrained(MODEL)
-    # config = AutoConfig.from_pretrained(MODEL)
-    # # PT
-    # model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-    # #%%
-    # text = "Covid cases are increasing fast!"
-    # text = preprocess(text)
-    # #%%
-    # encoded_input = tokenizer(text, return_tensors='pt')
-    # output = model(**encoded_input)
-    # scores = output[0][0].detach().numpy()
-    # scores = softmax(scores)
-    # # %%
-    # ranking = np.argsort(scores)
-    # ranking = ranking[::-1]
-    # #%%
-    # for i in range(scores.shape[0]):
-    #     l = config.id2label[ranking[i]]
-    #     s = scores[ranking[i]]
-    #     print(f"{i+1}) {l} {np.round(float(s), 4)}")
-    # # %%
-    # ### use pip 
-    # pipe = pipeline(task="text-classification",
-    #                 model = MODEL,
-    #                 tokenizer=MODEL,return_all_scores=True)#,device=0)
-    # #%%
-    # print(pipe(text))
-    # print(transform_pipe_results(pipe(text)[0]))
-    # #%%
---
+++ examples/tweet_classification/src/sentiment_translate_scripts.sh
@@ -0,0 +1,67 @@
+# scripts 
+
+
+## v2 results 
+
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Uruguay__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Uruguay__comments_sentiment.csv
+
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Paraguay__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Paraguay__comments_sentiment.csv
+
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Argentina__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Argentina__comments_sentiment.csv
+
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Brazil__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Brazil__comments_sentiment.csv
+
+
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Australia__comments.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Australia__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Canada__comments.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Canada__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Chile__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Chile__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Germany__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Germany__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Italy__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Italy__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Japan__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Japan__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Mexico__comments_eng.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Mexico__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/UK__comments.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/UK__comments_sentiment.csv
+# python ../inference_simple_translated_data.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/US__comments.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/US__comments_sentiment.csv
+
+### V3 results 
+
+python ../inference_simple_translated_data.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Australia__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Australia__comments_sentiment.csv
+
+python ../inference_simple_translated_data.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Canada__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/Canada__comments_sentiment.csv
+
+python ../inference_simple_translated_data.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/UK__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/UK__comments_sentiment.csv
+
+python ../inference_simple_translated_data.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/US__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_sentiment/US__comments_sentiment.csv
