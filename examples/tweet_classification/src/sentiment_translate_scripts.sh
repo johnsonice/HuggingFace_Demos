c1068e86f0df3f158ded1a947ebe3f86ae84bd3f
johnsonice <huangchengyu16@gmail.com>
2024-05-05 11:37:48 -0400

    update

---
+++ examples/tweet_classification/src/sentiment_translate_scripts.sh
@@ -0,0 +1,69 @@
+# scripts 
+# python translation.py \
+#     -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Germany__comments.csv \
+#     -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Germany__comments_eng.csv \
+#     -src de_DE
+#     -tagt en_XX
+#
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Mexico__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Mexico__comments_eng.csv \
+    -src es_XX
+    -tagt en_XX
+
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Argentina__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Argentina__comments_eng.csv \
+    -src es_XX \
+    -tagt en_XX
+
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Brazil__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Brazil__comments_eng.csv \
+    -src pt_XX \
+    -tagt en_XX
+
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Chile__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Chile__comments_eng.csv \
+    -src es_XX \
+    -tagt en_XX
+
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Italy__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Italy__comments_eng.csv \
+    -src it_IT
+    -tagt en_XX
+
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Japan__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Japan__comments_eng.csv \
+    -src ja_XX \
+    -tagt en_XX
+
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Paraguay__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Paraguay__comments_eng.csv \
+    -src es_XX \
+    -tagt en_XX
+
+python translation.py \
+    -in_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments/Uruguay__comments.csv \
+    -out_file /data/chuang/Twitter_Data/Twitter_API_PY/Data/Tweet_by_user/chunks/User_Comments_english/Uruguay__comments_eng.csv \
+    -src es_XX \
+    -tagt en_XX
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
